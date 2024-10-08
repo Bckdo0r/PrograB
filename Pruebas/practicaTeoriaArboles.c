de9d1a148dd157b792f5d7e6598b1e68873fe7aa
genesis <johndoe@example.com>
2024-10-08 12:15:09 -0300

    claseArboles

---
+++ Pruebas/practicaTeoriaArboles.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+//!1-7-12-5-17-10-16-59-26-24-33-50-30
+//*1-7-10-12-5-17-30-50-16-33-24-59-26
+
+//1-7-12-5-17-10-16-59-26-24-33-50-30
+
+typedef struct nodo{
+   int dato;
+   struct nodo *izq,*der;
+} nodo;
+
+typedef nodo *Arbol;
+
+void cantHojas(Arbol A, int *cont){
+   if (A != NULL){
+      if (A->izq == NULL && A->der == NULL)
+         (*cont)++;
+      else{
+         cantHojas(A->izq,cont);
+         cantHojas(A->der,cont);      
+      }   
+   }
+}
+
+int contHojas(Arbol A){
+   if (A != NULL){
+      if (A->izq == NULL && A->der == NULL)
+         return 1 + contHojas(A);
+
+   }
+}
+
+int main() {
+
+
+   return 0;
+}
