7fb9545d41ee60f2e55c0bb533f5de462865c2e2
Genesis <Genesis>
2024-10-25 11:45:51 -0300

    ej8

--- Practica8/ejercicio8.c
+++ Practica8/ejercicio8.c
@@ -14,7 +14,6 @@ d) Generar un vector de registros con grado y vértice para aquellos vértices, 
 typedef int Vec[N];
 
 // a)
-int grado();
 void crearVec(TMat, Vec, int, int);
 
 // b)
@@ -22,20 +21,31 @@ int _verticeConMayorGradoEntrada(TMat, int, int, int, int, int);
 char verticeConMayorGradoEntrada(TMat);
 
 // c)
-int cumple(TMat, int, int, int);
+int cumple(TMat, int,int,int,int,int);
 
 int main()
 {
+    int X = 2;
     TMat mat = {
         {0, 0, 1, 1, 0, 1, 0},
         {0, 0, 1, 1, 0, 0, 0},
         {1, 1, 0, 0, 0, 1, 0},
-        {1, 1, 0, 1, 1, 1, 1},
+        {1, 1, 0, 0, 1, 1, 1},
         {0, 0, 0, 1, 0, 0, 1},
         {1, 0, 1, 1, 0, 0, 1},
         {0, 0, 0, 1, 1, 1, 0}};
 
-    Vec vec;
+    int matriz[N][N] = {
+        {0, 4, 0, 7, 3, 0, 5}, // A -> B, D, E, G
+        {0, 0, 5, 0, 6, 1, 0}, // B -> C, E, F
+        {0, 0, 0, 2, 8, 0, 3}, // C -> D, E, G
+        {0, 0, 0, 0, 9, 4, 0}, // D -> E, F
+        {0, 0, 0, 0, 0, 7, 2}, // E -> F, G
+        {0, 0, 0, 0, 0, 0, 6}, // F -> G
+        {0, 0, 0, 0, 0, 0, 0}  // G -> (Ninguno)
+    };    
+
+    Vec vec = {0,0,0,0,0,0,0};
 
     crearVec(mat, vec, 0, 0);
 
@@ -43,6 +53,7 @@ int main()
         printf("%c: %d\n", 'A' + i, vec[i]);
 
     printf("El vértice con mayor grado de entrada es: %c\n", verticeConMayorGradoEntrada(mat));
+    printf("Todos los vertices del grafo %s cumplen la condicion pedida.\n",cumple(matriz,0,0,N-1,N-1,X) ? "SI":"NO");
 
     return 0;
 }
@@ -81,3 +92,16 @@ int _verticeConMayorGradoEntrada(TMat mat, int i, int j, int grado, int maxVerti
 }
 
 // c)
+int cumple(TMat M,int i,int j,int f,int c,int X){
+    if (i > N && c < 1) //recorri toda la media matriz;
+        return 1;
+    else{
+        
+        if (j>N)
+            return M[i][j] > X ? cumple(M,i+1,i+1,f,c,X) : cumple(M,i,j+1,f,c,X);
+        else if (f>0)
+            return M[f][c] > X ? cumple(M,i,j,c-1,c-1,X) : cumple(M,i,j,f-1,c,X);
+        else
+            return 0; 
+    }
+}
